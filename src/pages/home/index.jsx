783c9a0f34622e5d68b73bb6b0d3a5e10ccfe81e
Richard <r.barnett091@yahoo.com>
2023-01-08 18:49:25 -0500

    feat: added react router to the project

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { css } from '@emotion/react';
 
+import Button from '../../components/button/button'
+
 const styles = {
-    textColor: css({
-        color: 'blue'
-    })
+
 }
 
 const Home = () => {
+    const [count, setCount] = useState(0);
+
+    // Primary Button Method
+    const increment = () => {
+        setcount(count + 1);
+    }
+
+    // Secondary Button Method
+    const decrement = () => {
+        setcount(count - 1);
+    }
+
+    // Default Button Method
+    const reset = () => {
+        setcount(0);
+    }
+
     return (
-        <div css={styles.textColor}>Home</div>
+        <div>
+            <h1>Counter</h1>
+            <div>You have clicked {count} times!!</div>
+            <Button>Hello</Button>
+        </div>
     )
 }
 
